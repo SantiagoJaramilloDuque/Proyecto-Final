2e6e37ba90c2d40e886e7a13838e01988b71655e
Santiago Jaramillo Duque <s.jaramillo4@utp.edu.co>
2025-05-25 16:10:20 -0500

    Getters y setter

--- model/doctor.py
+++ model/doctor.py
@@ -7,14 +7,26 @@ class Doctor:
     @property
     def doctor_id(self):
         return self.__doctor_id
-    
+
+    @doctor_id.setter
+    def doctor_id(self, value):
+        self.__doctor_id = value
+
     @property
     def nombre(self):
         return self.__nombre
-    
+
+    @nombre.setter
+    def nombre(self, value):
+        self.__nombre = value
+
     @property
     def especialidad(self):
         return self.__especialidad
-    
+
+    @especialidad.setter
+    def especialidad(self, value):
+        self.__especialidad = value
+
     def __str__(self):
         return f"Doctor(ID: {self.doctor_id}, Nombre: {self.nombre}, Especialidad: {self.especialidad})"
--- model/hospital.py
+++ model/hospital.py
@@ -8,13 +8,24 @@ class Hospital:
     @property
     def nombre(self):
         return self.__nombre
-    
+
+    @nombre.setter
+    def nombre(self, value):
+        self.__nombre = value
+
     @property
     def doctores(self):
         return self.__doctores
-    
+
+    @doctores.setter
+    def doctores(self, lista_doctores):
+        if isinstance(lista_doctores, list) and all(isinstance(d, Doctor) for d in lista_doctores):
+            self.__doctores = lista_doctores
+        else:
+            raise ValueError("La lista de doctores debe contener solo objetos de tipo Doctor.")
+
     def agregar_doctor(self, doctor: Doctor):
         self.__doctores.append(doctor)
-    
+
     def __str__(self):
         return f"Hospital(Nombre: {self.nombre}, Doctores: {len(self.doctores)})"
